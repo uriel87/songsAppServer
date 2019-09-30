0ffb2b18abe9c948001b4652a652f8ce68312b5e
uriel87 <urielch87@gmail.com>
2019-09-30 18:03:27 +0300

    add user form to db fixed

--- config/express.js
+++ config/express.js
@@ -15,9 +15,14 @@ module.exports = function() {
 
 	app.use('/', express.static('./public'));
 
-	app.use(function(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+	  app.use(function(req, res, next) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+		res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		if(req.method === 'OPTIONS') {
+			return res.sendStatus(200)
+		}
 		next();
 	  });
 
--- index.js
+++ index.js
@@ -6,8 +6,12 @@ var express = require('./config/express');
 var app = express();
 
 // Use the Express application instance to listen to the port
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 
 var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port + ' ..');
 });
+
+// const server = app.listen(process.env.PORT || 8000, function() {
+//     console.log('Express server listening on port ' + server.address().port + ' ..');
+// });
